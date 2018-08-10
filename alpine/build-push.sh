763627757508999789ff6ddf9eba57bcbebca52b
brunneis <dev@brunneis.com>
2018-08-10 20:59:49 +0200

    Fixed confluent-kafka version, Python 3.7 for Ubuntu

--- alpine/build-push.sh
+++ alpine/build-push.sh
@@ -2,5 +2,6 @@
 docker build \
 --build-arg CATENAE_VERSION=$(cat CATENAE_VERSION) \
 --build-arg LIBRDKAFKA_VERSION=$(cat LIBRDKAFKA_VERSION) \
+--build-arg CONFLUENT_KAFKA_VERSION=$(cat CONFLUENT_KAFKA_VERSION) \
 -t catenae/link:alpine .
 docker push catenae/link:alpine
--- ubuntu/build-push.sh
+++ ubuntu/build-push.sh
@@ -2,6 +2,7 @@
 docker build \
 --build-arg CATENAE_VERSION=$(cat CATENAE_VERSION) \
 --build-arg LIBRDKAFKA_VERSION=$(cat LIBRDKAFKA_VERSION) \
+--build-arg CONFLUENT_KAFKA_VERSION=$(cat CONFLUENT_KAFKA_VERSION) \
 -t catenae/link:ubuntu .
 docker tag catenae/link:ubuntu catenae/link:latest
 docker push catenae/link:ubuntu
